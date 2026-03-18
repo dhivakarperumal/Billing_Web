9887cb25c3c6ab830ca7a0ff8a3819e4ee91200b
Dhivakar <dhivakarp305@gmail.com>
2026-03-18 10:14:43 +0530

    fix it index js

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import "./src/config/env.js";
 import express from "express";
 import cors from "cors";
+
 import authRouter from "./src/routers/authRouter.js";
 
 const app = express();
@@ -8,41 +9,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Request Logger
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
+// Auth Routes
 app.use("/api/auth", authRouter);
 
-app.get("/debug-routes", (req, res) => {
-  const routes = [];
-  app._router.stack.forEach((middleware) => {
-    if (middleware.route) {
-      routes.push(`${Object.keys(middleware.route.methods)} ${middleware.route.path}`);
-    } else if (middleware.name === 'router') {
-      middleware.handle.stack.forEach((handler) => {
-        if (handler.route) {
-          routes.push(`${Object.keys(handler.route.methods)} ${middleware.regexp} ${handler.route.path}`);
-        }
-      });
-    }
-  });
-  res.json(routes);
-});
-
+// Test Route
 app.get("/", (req, res) => {
   res.send("Backend Running");
 });
 
-// 404 handler
+// 404 Handler
 app.use((req, res) => {
-  console.log(`404: ${req.method} ${req.url}`);
-  res.status(404).json({ message: `Route ${req.method} ${req.url} not found` });
+  res.status(404).json({ message: "Route not found" });
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
